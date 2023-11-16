ad73f90fed4e742ee1f683cb1d81d0bd55553fc6
Wenjin09 <wenjinli@ucsb.edu>
2023-11-15 20:39:18 -0800

    init

--- app/src/main/java/com/example/stockpulse/MainActivity.java
+++ app/src/main/java/com/example/stockpulse/MainActivity.java
@@ -6,9 +6,27 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String APPNAME = "StockPulse";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPuase() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
