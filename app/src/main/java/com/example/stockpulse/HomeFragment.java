b11d34e96753b21de0ef259acae01722d121d657
alvin0220 <alvin0220@gmail.com>
2023-12-10 23:29:02 -0800

    changed listview into recycleView

--- app/src/main/java/com/example/stockpulse/HomeFragment.java
+++ app/src/main/java/com/example/stockpulse/HomeFragment.java
@@ -3,23 +3,18 @@ package com.example.stockpulse;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,17 +22,17 @@ import java.util.List;
 import com.example.stockpulse.network.FinnhubAPIResponse;
 import com.example.stockpulse.network.YahooFinanceAPIResponse;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements RecyclerViewInterface {
 
     private View view;
-    private ListView stockListViewUI;
     private EditText userInputUI;
     private Button searchButtonUI;
-    String sampleData[] = {"data", "data", "data", "data", "data", "data", "data", "data", "data", "data"};
-    ArrayAdapter<String> arrayAdapter;
     private ToggleButton toggleButtonUI;
     private boolean isYahoo = false;
+    private RecyclerView homeListView;
+    private stockAdapter stockAdapter;
 
+    private List<stockItem> stockItemList;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -51,22 +46,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
-        stockListViewUI = view.findViewById(R.id.stockListLayout);
+
         userInputUI = view.findViewById(R.id.searchBarLayout);
         searchButtonUI = view.findViewById(R.id.searchButtonLayout);
         toggleButtonUI = view.findViewById(R.id.toggleButtonLayout);
 
-        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, sampleData);
-        stockListViewUI.setAdapter(arrayAdapter);
+        homeListView = view.findViewById(R.id.homeListLayout);
+        homeListView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        stockItemList = generateStockItem();
+        stockAdapter = new stockAdapter(stockItemList, this);
+        homeListView.setAdapter(stockAdapter);
 
-//        stockListViewUI.setOnClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                TextView temp = ((TextView) view);
-//                temp.setText(sampleData[position]);
-//                arrayAdapter.notifyDataSetChanged();
-//            }
-//        });
 
         toggleButtonUI.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -135,7 +125,18 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return view;
     }
+    public List<stockItem> generateStockItem() {
+        List<stockItem> stockItemList = new ArrayList<>();
+        stockItemList.add(new stockItem("AAPL", 195.71, 1.44));
+        stockItemList.add(new stockItem("AMZN", 147.42, 0.54));
+        stockItemList.add(new stockItem("GOOG",136.64,-1.81));
+        return stockItemList;
+    }
 
 
-
+    @Override
+    public void onItemClick(int position) {
+        Log.d("favList","List number:"+position);
+        Toast.makeText(getActivity(), "this is item number: " + position, Toast.LENGTH_SHORT).show();
+    }
 }
